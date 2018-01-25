ede6a53e145db2123048778f9b351d30372bd69c
gowrisankarb <lvenkat3@in.ibm.com>
2018-01-25 15:26:45 +0530

    rollback

--- TestAPI/Program.cs
+++ TestAPI/Program.cs
@@ -5,49 +5,21 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;// 1st line added
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
 namespace TestAPI
 {
-    //public class Program
-    //{
-    //    public static void Main(string[] args)
-    //    {
-    //        var config = new ConfigurationBuilder().AddEnvironmentVariables("").Build();
-    //        // 2nd line added
-
-    //        var url = config["ASPNETCORE_URLS"] ?? "http://*:8080";
-    //        // 3rd line added
-
-    //        BuildWebHost(args).Run();
-    //    }
-
-    //    public static IWebHost BuildWebHost(string[] args) =>
-    //        WebHost.CreateDefaultBuilder(args)
-    //            .UseStartup<Startup>()
-    //            .Build();
-    //}
-
     public class Program
     {
         public static void Main(string[] args)
         {
-            var config = new ConfigurationBuilder().AddEnvironmentVariables("").Build();
-            // 2nd line added
-
-            var url = config["ASPNETCORE_URLS"] ?? "http://*:8080";
-            // 3rd line added
-
-            var host = new WebHostBuilder()
-            .UseKestrel()
-            .UseContentRoot(Directory.GetCurrentDirectory())
-            .UseIISIntegration()
-            .UseStartup(url)
-            .UseUrls(url) // 4th line added
-            .Build();
-
-            host.Run();
+            BuildWebHost(args).Run();
         }
+
+        public static IWebHost BuildWebHost(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>()
+                .Build();
     }
 }
