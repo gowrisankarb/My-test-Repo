7af750227750235d97deae6087dbec56ba3b54ae
gowrisankarb <lvenkat3@in.ibm.com>
2018-01-31 17:17:53 +0530

    new values

--- TestAPI/Controllers/ValuesController.cs
+++ TestAPI/Controllers/ValuesController.cs
@@ -13,7 +13,7 @@ namespace TestAPI.Controllers
         [HttpGet]
         public IEnumerable<string> Get()
         {
-            return new string[] { "value1", "value2" };
+            return new string[] { "service", "consumed" };
         }
 
         // GET api/values/5
